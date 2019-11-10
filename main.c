fa399582db06c6cd2f8c5536e31ae3a256f3eac2
PauloRolim <pauloerb@hotmail.com>
2019-11-10 20:03:33 -0300

    Adicionando alguns comentarios

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ Color corPadrao={255,255,255};
 Imagem imagem;
 Imagem * pontImagem=&imagem;
 
-
+// Caixa de diálogo que apresenta a mensagem de imagem salva
 static void dialogSalvar (GtkWidget *wid, GtkWidget *win){
   save();
   GtkWidget *dialog = NULL;
@@ -18,7 +18,7 @@ static void dialogSalvar (GtkWidget *wid, GtkWidget *win){
   gtk_widget_destroy (dialog);
 }
 
-
+// Caixa de dialogo que apresenta a mensagem imagem gerada
 static void dialogGerar (GtkWidget *wid, GtkWidget *win){
   newImagem();
   GtkWidget *dialog = NULL;
@@ -29,6 +29,7 @@ static void dialogGerar (GtkWidget *wid, GtkWidget *win){
   gtk_widget_destroy (dialog);
 }
 
+// Função para salver o arquivo de imagem .PPM
 void save(){
 
     FILE *file = fopen("imagem.ppm","w");
@@ -88,12 +89,12 @@ int main (int argc, char *argv[]){
   GtkWidget *win = NULL;
   GtkWidget *vbox = NULL;
 
-  /* Initialize GTK+ */
+  // Inicialização do GTK+
   g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
   gtk_init (&argc, &argv);
   g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
 
-  /* Create the main window */
+  // Cria a tela principal no GTK+
   win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_container_set_border_width (GTK_CONTAINER (win), 8);
   gtk_window_set_title (GTK_WINDOW (win), "Projeto ITP");
@@ -103,23 +104,26 @@ int main (int argc, char *argv[]){
   gtk_widget_realize (win);
   g_signal_connect (win, "destroy", gtk_main_quit, NULL);
 
-  /* Create a vertical box with buttons */
+  // Cria uma caixa vertical com botões
   vbox = gtk_vbox_new (TRUE, 6);
   gtk_container_add (GTK_CONTAINER (win), vbox);
 
+  // Botão 01 criar imagem
   button = gtk_button_new_from_stock ("Gerar Imagem");
   g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (dialogGerar), (gpointer) win);
   gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
 
+  // Botaão 02 Salvar imagem
   button = gtk_button_new_from_stock ("Salvar Imagem");
   g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (dialogSalvar), (gpointer) win);
   gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
 
+  // Botão 02 sair da aplicação
   button = gtk_button_new_from_stock ("Sair");
   g_signal_connect (button, "clicked", gtk_main_quit, NULL);
   gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
 
-  /* Enter the main loop */
+  // Mantem um loop para persistir a janela da aplicação aberta até o encerramento
   gtk_widget_show_all (win);
   gtk_main ();
 
